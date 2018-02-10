6304f31d969285a58f918a27111eb6cc1a692522
Simon Howard <fraggle@soulsphere.org>
2018-02-10 20:18:59 +0000

    Add rocket launcher.
    Also add commented-out BFG.

--- examples/smooth.py
+++ examples/smooth.py
@@ -227,6 +227,71 @@ weaponinfo[wp_chainsaw].update(states.parse(dehfile.free_states(), """
                 Goto Ready
 """))
 
+weaponinfo[wp_missile].clear()
+weaponinfo[wp_missile].ammo = am_misl
+dehfile.assign_sprites(("MISG", "PKRL", "PKRF"))
+weaponinfo[wp_missile].update(states.parse(dehfile.free_states(), """
+        Ready:
+                MISG A 1 A_WeaponReady
+                Loop
+        Deselect:
+                MISG A 1 A_Lower
+                Loop
+        Select:
+                MISG A 1 A_Raise
+                Loop
+        Fire:
+                PKRL A 2
+                PKRL A 4 A_GunFlash
+                PKRL B 2
+                PKRL D 3 A_FireMissile
+                PKRL C 3
+                PKRL BE 2
+                PKRL FG 1
+                PKRL A 0 A_ReFire
+                Goto Ready
+        Flash:
+                PKRF A 3 Bright A_Light1
+                PKRF B 2 Bright
+                PKRF C 2 Bright A_Light2
+                PKRF DE 3 Bright
+                Goto S_LIGHTDONE
+"""))
+
+#weaponinfo[wp_bfg].clear()
+#weaponinfo[wp_bfg].ammo = am_cell
+#dehfile.assign_sprites(("BFGG", "BFGN", "BFGF"))
+#weaponinfo[wp_bfg].update(states.parse(dehfile.free_states(), """
+#        Ready:
+#                BFGG A 1 A_WeaponReady
+#                Loop
+#        Deselect:
+#                BFGG A 1 A_Lower
+#                Loop
+#        Select:
+#                BFGG A 1 A_Raise
+#                Loop
+#        Fire:
+#                BFGG A 20 A_BFGsound
+#                BFGG A 2 A_GunFlash
+#                BFGN A 8
+#                BFGN A 10 A_FireBFG
+#                BFGN A 1 A_ReFire
+#                BFGG A 19 A_ReFire
+#                goto Ready
+#        Flash:
+#		BFGF A 11 A_Light1
+#		BFGF B 6 A_Light2
+#		Goto S_LIGHTDONE
+#            #   BFGW A 2 bright
+#            #   BFGX HGFE 1 bright A_Light2
+#            #   BFGX DCBA 1 bright A_Light2
+#            #   TNT1 A 0 A_Light0
+#            #   BFGY ABCDE 1 bright
+#            #   BFGY FGHIJKL 1
+#            #   Goto LightDone
+#"""))
+
 dehfile.save("smooth.deh")
 
 # Any left over?
