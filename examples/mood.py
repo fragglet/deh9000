74bd5c61174cba336746de1e070b4dee3135d933
Jonathan Dowland <jon@dow.land>
2018-06-15 20:36:26 +0100

    adjust boolean logic; reformat for tabs

--- examples/mood.py
+++ examples/mood.py
@@ -10,30 +10,30 @@ from deh9000 import *
 
 # pacifist monsters
 for monster in mobjinfo:
-    if not (monster.flags & MF_COUNTKILL or monster == mobjinfo[MT_SKULL]):
-        continue
+	if not (monster.flags & MF_COUNTKILL) and monster != mobjinfo[MT_SKULL]:
+		continue
 
-    if monster.missilestate:
-        states[monster.missilestate].tics = 1
-        states[monster.missilestate].nextstate = monster.seestate
+	if monster.missilestate:
+		states[monster.missilestate].tics = 1
+		states[monster.missilestate].nextstate = monster.seestate
 
-    # necessary if melee and missile use different frames (e.g. revenant)
-    if monster.meleestate:
-        states[monster.meleestate].tics = 1
-        states[monster.meleestate].nextstate = monster.seestate
+	# necessary if melee and missile use different frames (e.g. revenant)
+	if monster.meleestate:
+		states[monster.meleestate].tics = 1
+		states[monster.meleestate].nextstate = monster.seestate
 
 projectile_weapons = [
-    MT_MISC25, # BFG 9000
-    MT_MISC27, # Rocket Launcher
-    MT_MISC28, # Plasma Gun
-    MT_SHOTGUN,
-    MT_SUPERSHOTGUN,
-    MT_CHAINGUN
+	MT_MISC25, # BFG 9000
+	MT_MISC27, # Rocket Launcher
+	MT_MISC28, # Plasma Gun
+	MT_SHOTGUN,
+	MT_SUPERSHOTGUN,
+	MT_CHAINGUN
 ]
 
 # make all weapons copies of +1 health potion (Except chainsaw)
 for weapon in projectile_weapons:
-    mobjinfo[weapon].spawnstate = mobjinfo[MT_MISC2].spawnstate
+	mobjinfo[weapon].spawnstate = mobjinfo[MT_MISC2].spawnstate
 
 miscdata.initial_bullets = 0
 
